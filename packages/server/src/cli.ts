918cdfd1c3272089e7b8c023d599b1fc513aca38
Yosuke Kurami <yosuke.kurami@gmail.com>
2018-09-17 23:02:40 +0900

    validate config

--- packages/server/src/cli.ts
+++ packages/server/src/cli.ts
@@ -4,6 +4,16 @@ import minimist from "minimist";
 import fs from "fs";
 import { bootstrap } from "./app";
 
+function valiidateConf(conf: any) {
+  if (!conf.languageServer) {
+    return { message: "key 'languageServer' is not set." };
+  }
+  if (!conf.languageServer.command) {
+    return { message: "key 'languageServer.command' is not set." };
+  }
+  return {};
+}
+
 function main() {
 
   const argv = minimist(process.argv.slice(2), {
@@ -21,6 +31,12 @@ function main() {
     return;
   }
   const config = require("js-yaml").safeLoad(fs.readFileSync(configFilename, "utf8"));
+
+  const { message } = valiidateConf(config);
+  if (message) {
+    console.error(`Config file "${configFilename}":`, message);
+    return process.exit(1);
+  }
 
   bootstrap({
     port: 4000,
