b62cc1cc186e384e41bfa50d5c172f69bce2f816
Yosuke Kurami <yosuke.kurami@gmail.com>
2018-09-19 05:17:17 +0900

    fix python script

--- deployment_scripts/gen_ingress.py
+++ deployment_scripts/gen_ingress.py
@@ -1,15 +1,13 @@
 #!/usr/bin/env python2.7
 import json
 
-f = open('deployment_scripts/services.json')
-contents = json.loads(f.read())
+contents = json.loads(open('deployment_scripts/services.json').read())
 
-names = [item["metadata"]["name"] for item in contents["items"]]
-
-specs = ['''      - path: /master
+rule_tmpl = '''      - path: /{branch}
         backend:
           serviceName: {name}
-          servicePort: 4000'''.format(name=name) for name in names]
+          servicePort: 4000'''
+specs = [rule_tmpl.format(name=item['metadata']['name'], branch=item['metadata']['labels']['suffix']) for item in contents['items']]
 
 print open('k8s/ingress.tmpl.yml').read()
 print '\n'.join(specs)
