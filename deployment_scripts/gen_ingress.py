91221468488bf60d7b67b6127a5b53d3581a1664
Yosuke Kurami <yosuke.kurami@gmail.com>
2018-09-19 05:28:10 +0900

    modify script

--- deployment_scripts/gen_ingress.py
+++ deployment_scripts/gen_ingress.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python2.7
+
 import json
 
 contents = json.loads(open('deployment_scripts/services.json').read())
@@ -7,7 +8,13 @@ rule_tmpl = '''      - path: /{branch}
         backend:
           serviceName: {name}
           servicePort: 4000'''
-specs = [rule_tmpl.format(name=item['metadata']['name'], branch=item['metadata']['labels']['suffix']) for item in contents['items']]
+
+specs = [
+    rule_tmpl.format(
+        name=item['metadata']['name'],
+        branch=item['metadata']['labels']['suffix'])
+    for item in contents['items'] if item['metadata']['name'] != 'web-master'
+]
 
 print open('k8s/ingress.tmpl.yml').read()
 print '\n'.join(specs)
